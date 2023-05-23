68c06526d39d1886e6d78ab5bce33f9a2af98e73
Igor Coelho <xh4rdzn@gmail.com>
2023-05-22 23:08:43 -0300

    not finished

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,109 @@
-const btnPlay = document.querySelector('.play')
-const btnStop = document.querySelector('.stop')
-const btnMore = document.querySelector('.more')
-const btnLess = document.querySelector('.less')
 const btnFlorest = document.querySelector('#florest')
 const btnRain = document.querySelector('#rain')
 const btnCoffe = document.querySelector('#coffe')
 const btnFire = document.querySelector('#fire')
+const btnPlay = document.querySelector('.play')
+const btnStop = document.querySelector('.stop')
+const btnMore = document.querySelector('.more')
+const btnLess = document.querySelector('.less')
+const minutesDisplay = document.querySelector('#minutes')
+const secondsDisplay = document.querySelector('#seconds')
+let minutes = Number(minutesDisplay.textContent)
+let seconds = Number(secondsDisplay.textContent)
+let timerTimeOut
 
+
+function lessMinutes () {
+  let newMinutes = Number(minutesDisplay.textContent) - 5
+  minutesDisplay.textContent = Number(newMinutes)
+
+  if (newMinutes < 0) {
+    resetTimer()
+  }
+  return
+}
+
+function moreMinutes () {
+  let newMinutes = Number(minutesDisplay.textContent) + 5
+  minutesDisplay.textContent = newMinutes 
+  return 
+  }
+
+
+function updateTimerDisplay(minutes, seconds) {
+  minutesDisplay.textContent = String(minutes).padStart(2, "0")
+  secondsDisplay.textContent = String(seconds).padStart(2, "0")
+}
+
+function resetTimer() {
+  updateTimerDisplay(minutes, 0)
+  clearTimeout(timerTimeOut)
+}
+
+function countdown(){
+  timerTimeOut = setTimeout(function() {
+    let seconds =  Number(secondsDisplay.textContent)
+    let minutes = Number(minutesDisplay.textContent)
+
+    updateTimerDisplay(minutes, 0)
+
+    if (minutes <= 0) {
+      resetTimer()
+      return
+    }
+
+    if( seconds <= 0 ) {
+      seconds = 60
+      --minutes
+    }
+
+    updateTimerDisplay(minutes, String(seconds - 1))
+
+    countdown()
+  }, 1000)
+}
+
+btnPlay.addEventListener('click', function() {
+  countdown()
+})
+
+
+btnStop.addEventListener('click', function() {
+  resetTimer()
+})
+
+btnMore.addEventListener('click', function() {
+  moreMinutes()
+})
+
+btnLess.addEventListener('click', function () {
+  lessMinutes()
+})
 
 btnCoffe.addEventListener('click', () => {
   btnCoffe.classList.toggle('active')
   btnFire.classList.remove('active')
+  btnRain.classList.remove('active')
   btnFlorest.classList.remove('active')
+})
+
+btnRain.addEventListener('click', () => {
+  btnRain.classList.toggle('active')
+  btnFire.classList.remove('active')
+  btnCoffe.classList.remove('active')
+  btnFlorest.classList.remove('active')
+})
+
+btnFlorest.addEventListener('click', () => {
+  btnFlorest.classList.toggle('active')
+  btnFire.classList.remove('active')
+  btnCoffe.classList.remove('active')
   btnRain.classList.remove('active')
 })
 
 btnFire.addEventListener('click', () => {
   btnFire.classList.toggle('active')
-  btnCoffe.classList.remove('active')
-  btnFlorest.classList.remove('active')
   btnRain.classList.remove('active')
-})
-
-btnFlorest.addEventListener('click', () => {
-  btnFlorest.classList.toggle('active')
   btnCoffe.classList.remove('active')
-  btnFire.classList.remove('active')
-  btnRain.classList.remove('active')
-})
-
-btnRain.addEventListener('click', () => {
-  btnRain.classList.toggle('active')
-  btnCoffe.classList.remove('active')
-  btnFire.classList.remove('active')
   btnFlorest.classList.remove('active')
 })
